7e928a7428241e57cafc3b33c666f6cb4e4af31f
irenekyc <gugu_irene@hotmail.com>
2020-04-20 12:05:28 -0500

    update DB

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3001;
 const app = express();
+const fs = require ('fs')
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
@@ -10,4 +11,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+const databasePath = path.join(__dirname, 'build/assets', 'db.json')
+
+app.get('/db', (req, res) => {
+    fs.readFile(databasePath, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.send(JSON.parse(data));
+    });
+});
 app.listen(port);
